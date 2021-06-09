fa16076173984495f31b5b17ff7a28e8229243da
salmansiddiqi95 <67078495+salmansiddiqi95@users.noreply.github.com>
2021-06-09 14:16:09 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,23 @@
+import time
+import paho.mqtt.client as paho
+broker="broker.hivemq.com"
+broker="iot.eclipse.org"
+#define callback
+def on_message(client, userdata, message):
+    time.sleep(1)
+    print("received message =",str(message.payload.decode("utf-8")))
+
+client= paho.Client("client-001") 
+client.on_message=on_message
+
+print("connecting to broker ",broker)
+client.connect("broker.hivemq.com")
+client.loop_start() 
+print("subscribing ")
+client.subscribe("house/bulb1")
+time.sleep(2)
+print("publishing ")
+client.publish("house/bulb1","Hello World")
+time.sleep(4)
+client.disconnect() 
+client.loop_stop() 
